8c12bd74dfad0e4043426316a89e2a1dac367aa2
bellahOchola <bellahkenya@gmail.com>
2020-01-20 11:54:06 +0300

    modify the home and base template files

--- cloth/urls.py
+++ cloth/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from django.urls  import path
+from django.urls  import path, include
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
