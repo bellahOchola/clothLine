0f9e1d6a04219dc8c4e05c8eba9af65754a5d3b3
bellahOchola <bellahkenya@gmail.com>
2020-01-20 11:28:41 +0300

    add signup and login pages and function

--- cloth/views.py
+++ cloth/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/signup.html', {'form':form})
+
